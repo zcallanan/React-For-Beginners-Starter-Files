28cd9e410459912bd1fd5fcd3f0d3741ba964b46
Zach Callanan <caveofmonsters@gmail.com>
2020-09-14 16:48:37 +0200

    Load sample fishes, save to state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   // Initial empty state
@@ -23,6 +24,11 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  // method to import fishes in Inventory
+  loadSampleFishes = () => {
+    this.setState( { fishes: sampleFishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -30,7 +36,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     );
   }
