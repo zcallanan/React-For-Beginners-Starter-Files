aede0b40a836aab35ead8999f6cc36dc88b0a876
Zach Callanan <caveofmonsters@gmail.com>
2020-09-15 16:39:20 +0200

    Fish component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
@@ -34,6 +35,12 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish
+              key={key}
+              details={this.state.fishes[key]}
+            />)}
+          </ul>
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
